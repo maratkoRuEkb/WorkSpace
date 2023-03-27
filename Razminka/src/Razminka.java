48dbb31544597aef6171b810ef68e017f1fff457
Marat Saburov <maratsaburov@yandex.ru>
2023-03-27 17:17:21 +0500

    НДС конвертер сделал консольную версию

--- Razminka/src/Razminka.java
+++ Razminka/src/Razminka.java
@@ -1,14 +1,21 @@
+import java.io.IOException;
+import java.io.*;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
-public class ConverterNDS {
-    public static void main(String[] args) {
+public class Razminka {
+        public static void main(String[] args) {
+        // 5. Напишите программу Java, которая принимает два числа в качестве входных данных и отображает произведение двух чисел.
+
         Scanner console = new Scanner(System.in);
         System.out.println("введите сумму с НДС");
         double number1 = console.nextDouble();
         double nds = 1.2;
         System.out.println("Сумма без НДС");
         double otvet = number1/nds;
+
+        //Вариант 1, с некоректным ответом типа 0,999999
+        System.out.println(otvet);
 
         //Вариант 2, с верным округлением до 2 знаков после запятой, 2 знака будут всегда.
         System.out.format("%.2f",otvet);
@@ -19,6 +26,15 @@ public class ConverterNDS {
         DecimalFormat decimalFormat = new DecimalFormat( "#.##" );
         String result = decimalFormat.format(value);
         System.out.println(result);
-    }
 
+        System.out.println("проверка математики");
+                System.out.println (19 % 12);
+                System.out.println (19 % -12);
+
+
+
+    }
 }
+
+
+
