a8932a65343b751965744a94d58e9cfafa602d23
Marat Saburov <maratsaburov@yandex.ru>
2023-03-31 17:03:33 +0500

    Signed-off-by: Marat Saburov <maratsaburov@yandex.ru>

--- ConverterNDS/src/ConverterNDS.java
+++ ConverterNDS/src/ConverterNDS.java
@@ -1,22 +1,65 @@
 import java.awt.*;
-import java.text.DecimalFormat;
+import java.awt.event.*;
 import java.util.Scanner;
 import javax.swing.*;
 
 
-public class ConverterNDS {
-    public static void main(String[] args) {
+public class ConverterNDS  {
+    private Object nds;
 
+    public static void main(String[] args) {
+        ConverterNDS  Converter = new ConverterNDS();
+        Converter.go();
+        double nds = 1.2;
+        //данные нужно будет хранить в связном списке
+
+
+
+
+
+        /
+
+    }//конец main
+
+    ActionListener reshit = new ActionListener()
+    //метод расчета
+    public double setOtvet() { 
+        
+        double otvet = number1/nds;
+        double number1;
+        String textNum= value.getText(); //получили цифру в виде текста надо привести его к Дабл
+        try {
+            number1 = Integer.doubleValue(textNum);
+            System.out.println(number1);
+        } catch (NumberFormatException e) {
+            System.err.println("Неправильный формат строки!");
+        }
+
+    }
+
+
+    
+
+    public void go () { //метод создающий интерфейс
         JFrame frame = new JFrame("Конвертер НДС");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300,300);
 
         //делаем верхнюю панель ГОТОВО
         JTextField value = new JTextField(10);// поле для ввода числа
+        //
+
+
+
         JButton button1 = new JButton("Вычислить");// кнопка запуска
+        button1.addActionListener(new ButtonStart());
+
         JPanel hightPanel = new JPanel();
         hightPanel.add(value);
         hightPanel.add(button1);
+
+
+
         //делаем нижнюю таблицу
         JPanel downPanel = new JPanel();
         JTable  table = new JTable (5,2);
@@ -28,19 +71,27 @@ public class ConverterNDS {
         frame.setVisible(true);
         //как заполнять таблицу данными???
 
-        //данные нужно будет хранить в связном списке
 
-        Scanner console = new Scanner(System.in);
-        System.out.println("введите сумму с НДС");
-        double number1 = console.nextDouble();
-        double nds = 1.2;
-        System.out.println("Сумма без НДС");
-        double otvet = number1/nds;
+    }// конец метода go
 
-        //Вариант 2, с верным округлением до 2 знаков после запятой, 2 знака будут всегда.
-        System.out.format("%.2f",otvet);
-        System.out.println();
+    //тут можно делать внутренний класс
+class ButtonStart implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
 
-    }
+            double column = e.getColumns();
+            //тут надо написать действие что будет происходить при нажатии кнопки ЗАПУСКАЕТ МЕТОД
+            //1.вводим в текстовое поле цифру
+            //2. нажимаем кнопку, берется цифра, проводится расчет
+            //3, результат передается в таблицу, результат выводится в таблице
 
-}
+        }
+    }// конец класса ButtonStart
+
+
+}// конец класса ConverterNDS
+
+
+
+
+
