d404efde3e6282f8e093de01178472bb15e91d8b
Marat Saburov <maratsaburov@yandex.ru>
2023-04-11 16:52:16 +0500

    change 11.04

--- Proba2/src/Proba2.java
+++ Proba2/src/Proba2.java
@@ -5,48 +5,51 @@ import java.io.Reader;
 
 public class Proba2 {
 
-    //double[][] massiv = new double[5][2];//создал массив данных для заполнения таблицы? 10 ячеек
+    //Object[][] massiv = new Object[5][2];//создал массив данных для заполнения таблицы 10 ячеек
 
+    static void  startGUI(Object data[][], Object columnNames[]){
+    //создание GUI
+    JFrame frame = new JFrame("Main window");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(300,300);
 
+    //делаем верхнюю панель
+    JTextField valueText = new JTextField(10);// поле для ввода числа
+    JButton button1 = new JButton("Отправить");// кнопка запуска
+    JPanel hightPanel = new JPanel();
+    hightPanel.add(valueText);
+    hightPanel.add(button1);
+
+    //делаем нижнюю таблицу
+    JPanel downPanel = new JPanel();
+
+    JTable table2 = new JTable(data, columnNames);// создаем таблицу JTable и передаем в нее данные
+    downPanel.add(table2);//дабавляем таблицу
+
+    frame.getContentPane().add(BorderLayout.NORTH, hightPanel);
+    frame.getContentPane().add(BorderLayout.CENTER, downPanel);
+    frame.setVisible(true);
+}
 
 
     public static void main(String[] args) {
         Proba2 Proba2 = new Proba2();
-        //создание GUI
-        JFrame frame = new JFrame("Main window");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300,300);
+        Object[][] data = new Object[5][2];
+        Object[] columnNames = {"С НДС","Без НДС"};
+        startGUI(data, columnNames);
 
-        //делаем верхнюю панель
-        JTextField valueText = new JTextField(10);// поле для ввода числа
-        JButton button1 = new JButton("Отправить");// кнопка запуска
-        JPanel hightPanel = new JPanel();
-        hightPanel.add(valueText);
-        hightPanel.add(button1);
-
-        //делаем нижнюю таблицу
-        JPanel downPanel = new JPanel();
-        //JTable  table = new JTable (5,1);
         // тестовые данные РАБОТАЕТ!!! только заголовки таблицы не работают
-        String[] columnNames = {"С НДС","Без НДС"};
-        Object[][] data = {
-                {"40000", 50000}, // так тоже можно РАБОТАЕТ
-                {"5500",  300.005},
-                {"5500",  Double.valueOf(300.548)},
+
+        // СОЗДАЛ МАССИВ С ОБЪЕКТАМИ Object И РАБОТАЕТ, принимает double без проблем!! ЗНАЧИТ В ТАКОЙ МАССИВ МОЖНО СОХРАНЯТЬ СВОИ ДАННЫЕ
 
 
-        };
+//        Object[][] data = {
+//                {"40000", 50000}, // так тоже можно РАБОТАЕТ
+//                {"5500",  300.005},
+//                {"5500",  Double.valueOf(300.548)},
+//        };//конец объявления массива
 
-        JTable table2 = new JTable(data, columnNames);
-        downPanel.add(table2);
-
-        frame.getContentPane().add(BorderLayout.NORTH, hightPanel);
-        frame.getContentPane().add(BorderLayout.CENTER, downPanel);
-        frame.setVisible(true);
-
-
-
-
+        //---
         double[] massiv = new double[5];//создал массив данных для заполнения таблицы? 10 ячеек
         massiv[0] = 40000;
         massiv[1] = 60000;
@@ -62,6 +65,6 @@ public class Proba2 {
         //table.setValueAt(massiv[1], 2,1 );
         //table.setValueAt(massiv[2], 3,1 );
 
-    }
+    }//конец main
 
 }
