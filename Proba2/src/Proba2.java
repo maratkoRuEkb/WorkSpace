5b67c58b50c4ac931bda3f811491c6ce681e1a6d
Marat Saburov <maratsaburov@yandex.ru>
2023-04-13 15:50:46 +0500

    nds version 0.1

--- Proba2/src/Proba2.java
+++ Proba2/src/Proba2.java
@@ -1,21 +1,22 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileInputStream;
 import java.io.Reader;
 
 public class Proba2 {
+    static double summaSNds = 0.0;
+    static double summaBezNds = 0.0;
 
-    //Object[][] massiv = new Object[5][2];//создал массив данных для заполнения таблицы 10 ячеек
-
-    static void  startGUI(Object data[][], Object columnNames[]){
-    //создание GUI
-    JFrame frame = new JFrame("Main window");
+    static void  startGUI(Object data[][], Object columnNames[]){//создание GUI
+    JFrame frame = new JFrame("Вычислитель без НДС");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(300,300);
 
     //делаем верхнюю панель
     JTextField valueText = new JTextField(10);// поле для ввода числа
-    JButton button1 = new JButton("Отправить");// кнопка запуска
+    JButton button1 = new JButton("Вычислить");// кнопка запуска
     JPanel hightPanel = new JPanel();
     hightPanel.add(valueText);
     hightPanel.add(button1);
@@ -29,42 +30,71 @@ public class Proba2 {
     frame.getContentPane().add(BorderLayout.NORTH, hightPanel);
     frame.getContentPane().add(BorderLayout.CENTER, downPanel);
     frame.setVisible(true);
-}
 
+    button1.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            //пишем что произойдет при нажатии кнопки
+            //берем сумму из текстового поля и делаем расчет
+            parsingText (valueText.getText()); //получаем дабл из текстового поля
+            summaBezNds = summaSNds/1.2;
+            System.out.println("получена сумма с НДС "+summaSNds);
+            System.out.println("получена сумма без НДС "+summaBezNds);
+            //в цикле переписываем массив со сдвигом вниз на строку и записываем новые данные
+            data [4] [0] = data [3] [0];
+            data [4] [1] = data [3] [1];
+
+            data [3] [0] = data [2] [0];
+            data [3] [1] = data [2] [1];
+
+            data [2] [0] = data [1] [0];
+            data [2] [1] = data [1] [1];
+
+            data [1] [0] = data [0] [0];
+            data [1] [1] = data [0] [1];
+
+            data [0] [0] = summaSNds;
+            data [0] [1] = summaBezNds;
+
+//
+//            data [0] [0]; data [0] [1]
+//            data [1] [0]; data [1] [1]
+//            data [2] [0]; data [2] [1]
+//            data [3] [0]; data [3] [1]
+//            data [4] [0]; data [4] [1]
+//
+            System.out.println("1 строка " + data[0][0] + ", "+ data [0] [1]);
+            System.out.println("2 строка " + data[1][0] + ", "+ data [1] [1]);
+            System.out.println("3 строка " + data[2][0] + ", "+ data [2] [1]);
+            System.out.println("4 строка " + data[3][0] + ", "+ data [3] [1]);
+            System.out.println("5 строка " + data[4][0] + ", "+ data [4] [1]);
+
+            //обновляем фрейм таблицы
+            downPanel.repaint(); // ПЕРЕРИСОВАТЬ нижнюю панель. работает
+            //frame.repaint(); // ПЕРЕРИСОВАТЬ все окно. работает
+            System.out.println("Все ок");
+        }
+    });//конец addActionListener
+}// конец метода startGUI
+
+    static void parsingText(String text){
+        String str2 = text;
+        try {
+            summaSNds = Double.parseDouble(str2);
+        } catch (NumberFormatException e) {
+            System.err.println("Неправильный формат строки!");
+        }// конец метода try
+    }
 
     public static void main(String[] args) {
         Proba2 Proba2 = new Proba2();
         Object[][] data = new Object[5][2];
         Object[] columnNames = {"С НДС","Без НДС"};
-        startGUI(data, columnNames);
 
-        // тестовые данные РАБОТАЕТ!!! только заголовки таблицы не работают
+        startGUI(data, columnNames); //запускаем ГУИ
+        // только заголовки таблицы не работают
 
-        // СОЗДАЛ МАССИВ С ОБЪЕКТАМИ Object И РАБОТАЕТ, принимает double без проблем!! ЗНАЧИТ В ТАКОЙ МАССИВ МОЖНО СОХРАНЯТЬ СВОИ ДАННЫЕ
-
-
-//        Object[][] data = {
-//                {"40000", 50000}, // так тоже можно РАБОТАЕТ
-//                {"5500",  300.005},
-//                {"5500",  Double.valueOf(300.548)},
-//        };//конец объявления массива
-
-        //---
-        double[] massiv = new double[5];//создал массив данных для заполнения таблицы? 10 ячеек
-        massiv[0] = 40000;
-        massiv[1] = 60000;
-        massiv[2] = 10000;
-        System.out.println(massiv[0]);
-        System.out.println(massiv[1]);
-        System.out.println(massiv[2]);
-
-        String s1 = new String("строка1");
-
-        //table.setValueAt(s1, 1,1 );
-        //System.out.println(table.isCellEditable(1,1));
-        //table.setValueAt(massiv[1], 2,1 );
-        //table.setValueAt(massiv[2], 3,1 );
-
+        System.out.println("Programm start");
     }//конец main
 
 }
