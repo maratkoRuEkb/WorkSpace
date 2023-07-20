7dd3b5803daf7c61717a8288be0264eed30dec75
Marat Saburov <maratsaburov@yandex.ru>
2023-07-20 10:04:22 +0500

    new Parser and ExercisesMini

---
+++ ExercisesMini/src/ExercisesMini.java
@@ -0,0 +1,14 @@
+public class ExercisesMini {
+    public static void main(String[] args) {
+        int a1 = 56;
+        int a2 = 9;
+        int a3 = 9;
+        int rezult1 =a1+a2*a3 ;
+        System.out.println("a = "+rezult1);
+        System.out.println((a1+a2)%a3);
+
+
+
+
+    }
+}
