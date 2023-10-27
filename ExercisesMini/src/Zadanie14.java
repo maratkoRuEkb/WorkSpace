8192c0021e9902d0c96d5f2ca94bb2c56d2b73e4
Marat Saburov <maratsaburov@yandex.ru>
2023-10-27 21:02:30 +0500

    add Zadanie 15

--- ExercisesMini/src/Zadanie14.java
+++ ExercisesMini/src/Zadanie14.java
@@ -1,9 +1,17 @@
 public class Zadanie14 {
     /*
-    Задание 14 -
+    Задание 14 - ВЫПОЛНЕНО
 Напишите Java-программу для печати американского флага на экране
      */
     public static void main(String[] args) {
-
+        System.out.println("США флажок");
+        System.out.println("******----------");
+        System.out.println("******----------");
+        System.out.println("******----------");
+        System.out.println("******----------");
+        System.out.println("----------------");
+        System.out.println("----------------");
+        System.out.println("----------------");
+        System.out.println("----------------");
     }
 }
